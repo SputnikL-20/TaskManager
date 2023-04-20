c78d9e95ee7cb2861cfe52bee2c28fe0750dab9b
unknown <sg_inbox@mail.ru>
2023-04-21 09:29:35 +1000

    update

---
+++ ВыборДаты/ВыборДаты/Form2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Data.SQLite;
+using System.Windows.Forms;
+
+namespace ВыборДаты
+{
+    public partial class Form2 : Form
+    {
+        private string dateTime;
+
+        public Form2()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            this.Text = "Выбранная дата: " + StaticData.DataBufferDATETIME + ' ' + StaticData.DataBufferTIME;
+            label1.Text = StaticData.DataBufferDATETIME + ' ' + StaticData.DataBufferTIME;
+            dateTime = StaticData.DataBufferDATE + ' ' + StaticData.DataBufferTIME;
+
+            Console.WriteLine("Дата и время для базы: {0}", dateTime);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sqlQuery = "INSERT " +
+                                "INTO taskManager ('date', 'notes') " +
+                              "VALUES ('" + dateTime + "', '" + textBox1.Text + "')";
+
+            using (SQLiteConnection con = new SQLiteConnection("Data Source=TestSQLite.sqlite"))
+            {
+                con.Open();
+
+                using (SQLiteCommand cmd = new SQLiteCommand(sqlQuery, con))
+                {
+                    try
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (SQLiteException ex)
+                    {
+                        Console.WriteLine("Ошибка добавления данных: " + ex.Message);
+                    }
+                }
+                con.Close();
+            }
+            this.Close();
+        }
+    }
+}
